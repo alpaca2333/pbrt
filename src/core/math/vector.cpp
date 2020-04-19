c8c27adac8ad2389f567596988cd8fb6bf980a42
qwertysun <qwertysun@tencent.com>
2020-04-19 20:25:25 +0800

    Add vector

---
+++ src/core/math/vector.cpp
@@ -0,0 +1,69 @@
+/*
+ * @brief
+ * @author  qwertysun
+ * @date    2020/04/19
+ */
+#include "vector.h"
+
+using namespace std;
+
+Vector3::Vector3(double a, double b, double c)
+{
+  e[0] = a;
+  e[1] = b;
+  e[2] = c;
+}
+
+Vector3 vsqrt(const Vector3& v)
+{
+  return {sqrt(v.e[0]), sqrt(v.e[1]), sqrt(v.e[2])};
+}
+
+
+Vector3 Refract(const Vector3 &income, const Vector3 &n, double r)
+{
+  if (income.Parallel(n)) return income;
+  double indot = income.Dot(n);
+  double cos1 = indot / income.Length() / n.Length();
+  Vector3 result;
+  if (indot < 0)
+  {
+    double sin1 = sqrt(1 - cos1 * cos1);
+    cos1 = -cos1;
+    double sin2 = sin1 / r;
+    double tan1 = abs(sin1 / cos1);
+    double tan2 = abs(sin2 / std::sqrt(1 - sin2 * sin2));
+    // 全反射
+    if (abs(sin2) >= 1)
+    {
+      return Reflect(income, n);
+    }
+    Vector3 n2 = n / n.Length() * income.Length() * cos1;
+    Vector3 delta = income + n2;
+    delta = delta / tan1 * tan2;
+    result = delta - n2;
+  }
+  else
+  {
+    double sin1 = sqrt(1 - cos1 * cos1);
+    double sin2 = sin1 * r;
+    double tan1 = abs(sin1 / cos1);
+    double tan2 = abs(sin2 / std::sqrt(1 - sin2 * sin2));
+    if (abs(sin2) >= 1)
+    {
+      return Reflect(income, n);
+    }
+    double cos1 = sqrt(1 - sin1 * sin1);
+    Vector3 n2 = -n / n.Length() * income.Length() * cos1;
+    Vector3 delta = income + n2;
+    delta = delta / tan1 * tan2;
+    result = delta - n2;
+  }
+  return result;
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector3& v)
+{
+  os << "{" << v.e[0] << ", " << v.e[1] << ", " << v.e[2] << "}";
+  return os;
+}
---
+++ src/core/math/vector.h
@@ -0,0 +1,183 @@
+/*
+ * @brief   Fundamental math definitions.
+ * @author  qwertysun
+ * @date    2020/04/19
+ */
+
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+
+
+#define VERY_SMALL_FLAOT 0.00000001
+
+/**
+ * Common 3-d vector definition
+ */
+class Vector3
+{
+public:
+  Vector3() = default;
+  Vector3(double a, double b, double c);
+  double e[3];
+  inline double& operator[](int i) { return e[i]; }
+  inline Vector3& operator=(const Vector3& vec);
+  inline Vector3 operator-() const;
+  friend inline Vector3 operator+(const Vector3& vec1, const Vector3& vec2);
+  friend inline Vector3 operator-(const Vector3& vec1, const Vector3& vec2);
+  inline Vector3 operator*(double k) const;
+  inline Vector3 operator*(const Vector3& vec);
+  inline Vector3 operator/(double k) const;
+  inline Vector3 operator/(const Vector3& vec);
+  inline Vector3& operator+=(const Vector3& vec);
+  inline Vector3& operator-=(const Vector3& vec);
+  inline Vector3& operator*=(double k);
+  inline Vector3& operator/=(double k);
+  inline double Dot(const Vector3 &vec) const;
+  inline Vector3 Cross(const Vector3 &vec) const;
+  inline Vector3 UnitVector() const;
+  inline double Length() const;
+  inline bool operator!=(const Vector3& v);
+  inline bool Parallel(const Vector3 &v) const;
+  friend std::ostream& operator<<(std::ostream& os, const Vector3& v);
+};
+
+
+Vector3 vsqrt(const Vector3& v);
+
+//=========================== inline function definitions ==============================
+
+inline double Dot(const Vector3 &vec1, const Vector3 &vec2)
+{
+  return vec1.Dot(vec2);
+}
+
+inline Vector3 Cross(const Vector3 &vec1, const Vector3 &vec2)
+{
+  return vec1.Cross(vec2);
+}
+
+inline Vector3 Vector3::operator-() const
+{
+  return {-e[0], -e[1], -e[2]};
+}
+
+inline Vector3 operator+(const Vector3& vec1, const Vector3& vec2)
+{
+  return {vec1.e[0] + vec2.e[0], vec1.e[1] + vec2.e[1], vec1.e[2] + vec2.e[2]};
+}
+
+inline Vector3 operator-(const Vector3& vec1, const Vector3& vec2)
+{
+  return {vec1.e[0] - vec2.e[0], vec1.e[1] - vec2.e[1], vec1.e[2] - vec2.e[2]};
+}
+
+inline Vector3 Vector3::operator*(double k) const
+{
+  return {e[0] * k, e[1] * k, e[2] * k};
+}
+
+inline Vector3 Vector3::operator/(double k) const
+{
+  return {e[0] / k, e[1] / k, e[2] / k};
+}
+
+inline Vector3 &Vector3::operator=(const Vector3 &vec)
+{
+  e[0] = vec.e[0];
+  e[1] = vec.e[1];
+  e[2] = vec.e[2];
+  return *this;
+}
+
+inline double Vector3::Dot(const Vector3 &vec) const
+{
+  return e[0] * vec.e[0] + e[1] * vec.e[1] + e[2] * vec.e[2];
+}
+
+inline Vector3 Vector3::Cross(const Vector3 &vec) const
+{
+  return {
+      e[1] * vec.e[2] - e[2] * vec.e[1],
+      e[2] * vec.e[0] - e[0] * vec.e[2],
+      e[0] * vec.e[1] - e[1] * vec.e[0]
+  };
+}
+
+inline double Vector3::Length() const
+{
+  return std::sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+}
+
+inline Vector3 Vector3::UnitVector() const
+{
+  return (*this) / Length();
+}
+
+inline Vector3 &Vector3::operator+=(const Vector3 &vec)
+{
+  e[0] += vec.e[0];
+  e[1] += vec.e[1];
+  e[2] += vec.e[2];
+  return *this;
+}
+
+inline Vector3 &Vector3::operator-=(const Vector3 &vec)
+{
+  e[0] -= vec.e[0];
+  e[1] -= vec.e[1];
+  e[2] -= vec.e[2];
+  return *this;
+}
+
+inline Vector3 &Vector3::operator*=(double k)
+{
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+  return *this;
+}
+
+inline Vector3 &Vector3::operator/=(double k)
+{
+  e[0] /= k;
+  e[1] /= k;
+  e[2] /= k;
+  return *this;
+}
+
+Vector3 Vector3::operator*(const Vector3 &vec)
+{
+  return {e[0] * vec.e[0], e[1] * vec.e[1], e[2] * vec.e[2]};
+}
+
+Vector3 Vector3::operator/(const Vector3 &vec)
+{
+  return {e[0] / vec.e[0], e[1] / vec.e[1], e[2] / vec.e[2]};
+}
+
+inline bool Vector3::operator!=(const Vector3 &v)
+{
+  return e[0] == v.e[0] && e[0] == v.e[1] && e[2] == v.e[2];
+}
+
+inline bool Vector3::Parallel(const Vector3 &v) const
+{
+  double delta = std::abs(v.UnitVector().Dot(UnitVector()) - 1);
+  return delta < VERY_SMALL_FLAOT;
+}
+
+inline Vector3 operator*(double k, const Vector3& vec)
+{
+  return vec * k;
+}
+
+inline Vector3 Reflect(const Vector3& income, const Vector3& n)
+{
+  Vector3 n2 = income.Dot(n) < 0 ? -n : n;
+  return income - 2 * income.Dot(n2) * n2;
+}
+
+Vector3 Refract(const Vector3 &income, const Vector3 &n, double r);
